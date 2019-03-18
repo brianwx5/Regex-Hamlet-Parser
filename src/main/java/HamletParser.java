ba21f32375812e2c42a5da6b423529128f105f7b
Brian Wong <brianwong@zipcoders-MacBook-Pro.local>
2019-03-17 21:46:10 -0400

    Finished

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,10 @@
+import javafx.scene.Parent;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -36,4 +40,27 @@ public class HamletParser {
         return hamletData;
     }
 
+    public String changeHamletToLeon(String text) {
+        // "\\bhamlet\\b"
+        String regex = "hamlet";
+        String replaceWith = "Leon";
+        Pattern p = Pattern.compile(regex,Pattern.CASE_INSENSITIVE);
+        text = p.matcher(text).replaceAll(replaceWith);
+        return text;
+
+    }
+
+    public String changeHoratioToTariq(String text) {
+        String regex = "horatio";
+        String replaceWith = "Tariq";
+        Pattern p = Pattern.compile(regex,Pattern.CASE_INSENSITIVE);
+        text = p.matcher(text).replaceAll(replaceWith);
+        return text;
+    }
+
+    public String changeHoratioAndHamletToLeonAndTariq(String text) {
+        text = changeHamletToLeon(text);
+        text = changeHoratioToTariq(text);
+        return text;
+    }
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,10 +16,58 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        //Given
+        HamletParser hamletParser = new HamletParser();
+        String word = "Hamlet, Leon";
+        String expected = "Leon, Leon";
+
+        //When
+        String actual = hamletParser.changeHamletToLeon(word);
+
+        //Then
+        Assert.assertEquals(expected,actual);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        //Given
+        HamletParser hamletParser = new HamletParser();
+        String word = "Horatio, horatio";
+        String expected = "Tariq, Tariq";
+
+        //When
+        String actual = hamletParser.changeHoratioToTariq(word);
+
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testChangeHoratioAndHamletToLeonAndTariq() {
+        //Given
+        HamletParser hamletParser = new HamletParser();
+        String word = "Horatio and Hamlet";
+        String expected = "Tariq and Leon";
+
+        //When
+        String actual = hamletParser.changeHoratioAndHamletToLeonAndTariq(word);
+
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testHamletParser() {
+        //Given
+        HamletParser hamletParser = new HamletParser();
+        String word = "Horatio and Hamlet";
+        String expected = "Tariq and Leon";
+
+        //When
+        String actual = hamletParser.changeHoratioAndHamletToLeonAndTariq(hamletText);
+
+        //Then
+        Assert.assertEquals(expected,actual);
     }
 
     @Test
@@ -28,4 +77,5 @@ public class HamletParserTest {
     @Test
     public void testFindHamlet() {
     }
+
 }
